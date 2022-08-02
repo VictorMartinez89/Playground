c36ed2f395faa039a365d9230351b700e9249119
VictorMartinez89 <vamartinezs@unal.edu.co>
2022-08-01 22:58:28 -0500

    Added changes on Business Logic

---
+++ FrontII/Clases/Clase1/Sincronica1_Ejercicio/ReadME.md
@@ -0,0 +1,15 @@
+Práctica Integradora
+
+Objetivo
+Realizar un script que permita sumar un array de números consecutivamente, de forma
+que se sume el primer número con el segundo y lo imprima por consola. Luego, tenemos
+que tomar este resultado y sumarle el tercer numero, y asi hasta terminar de recorrer el
+array
+
+
+Array de Entrada [1, 2, 4,  8]
+
+Valor Mostrado 1
+Valor Mostrado 1 + 2
+Valor Mostrado 3 + 4
+Valor Mostrado 7 + 8
--- FrontII/Clases/Clase1/Sincronica1_Ejercicio/index.js
+++ FrontII/Clases/Clase1/Sincronica1_Ejercicio/index.js
@@ -1,15 +1,24 @@
+function sumArr(arr){
+    let response  = "";
+    let total = 0;
+    
+    response = " [ " + arr + " ] " + "\n";
 
-
-function sumarArray(arr){
-    let resultado = 0;
-    arr.forEach(element => {
-       resultado  += element;
-    });
-    return resultado;
+    for(let i = 0; i< arr.length; i++){
+       let temp = arr[i];
+       if(i == 0){ 
+           response += temp + "\n";
+           total = temp;
+        }else{
+            response += total1, + " + " + temp + "\n";
+            total  += temp;
+       }
+    }
+    return response;
 }
 
 
 let arr =  prompt("Ingresar los numeros del array separado por comas").split(",");
 arr = arr.map(Number);
-let resultadoSuma = sumarArray(arr);
-alert("El resultado de la suma de los arrays fue : " + sumarArray(arr));
+let totalResult = sumArr(arr);
+alert("El resultado de la suma de los arrays fue : " + totalResult);
